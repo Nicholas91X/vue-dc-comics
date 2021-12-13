483f7d489cf28c624b3348c4c19bb48af1c335c2
Nicholas <romitinicholas@gmail.com>
2021-12-13 16:46:59 +0100

    struttura base

---
+++ src/components/Main.vue
@@ -0,0 +1,27 @@
+<template>
+    <main>
+        <h3> --> The content goes here  </h3>
+    </main>
+</template>
+
+<script>
+export default {
+    name: "Main"
+}
+</script>
+
+<style lang="scss">
+    @import "../assets/style/partials/variables.scss";
+    main {
+        background-color: $secondary-color;
+        height: 100px;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+
+        h3 {
+        color: $tertiary-color;
+        margin: 150px;
+        }
+    }
+</style>
